f50bf1b3802ccc617c41980eeb9c616864beb6ed
Felix Wolff <felix.wolff82@gmail.com>
2015-11-27 12:40:18 +0100

    Initial Commit

---
+++ Exercise1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise1
+//
+//  Created by Felix Wolff on 27.11.15.
+//  Copyright © 2015 brix cross media AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
